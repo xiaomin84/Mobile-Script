baa35f57dbab81725705d22d6883eb7103944219
Jianlong.Chen <jianlong.chen@deltaww.com>
2017-03-14 10:31:12 +0800

    fisrt version

---
+++ version.groovy
@@ -0,0 +1,7 @@
+@Grab(group='org.slf4j', module='slf4j-simple', version='1.7.21')
+@Grab(group='org.ajoberstar', module='grgit', version='1.8.0')
+
+import org.ajoberstar.grgit.*
+def grgit = Grgit.open(dir:'/home/jenkins/workspace/justtest')
+def map = [COMPUTE_VAR2: grgit.head().abbreviatedId]
+print map
